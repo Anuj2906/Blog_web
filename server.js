e5959f219cf3e99eb1d784ce07fa2b4600a05eee
Anuj2906 <anujgupta8955@gmail.com>
2022-07-15 16:54:26 +0530

    Some Changes in Files and added some files.

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Article = require('./models/article')
+const Article = require('./models/article');
 const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
 const app =  express();
 
 mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true});
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: false}))
-
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 
 
 app.get('/', async (req,res)=>{
     const articles = await Article.find().sort({createdAt: 'desc'});
     res.render('articles/index', {text: articles})
 });
-app.listen(3000)
+
 
 app.use('/articles',articleRouter)
+app.listen(3000)
