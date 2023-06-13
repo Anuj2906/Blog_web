41b1c86798fc44897d1ad122b39b55e2297d12a3
Anuj2906 <anujgupta8955@gmail.com>
2023-06-13 16:13:48 +0530

    some change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(methodOverride('_method'));
 
 app.get('/', async (req,res)=>{
     const articles = await Article.find().sort({createdAt: 'desc'});
-    res.render('articles/index', {text: articles});
+    res.render('index', {text: articles});
 });
 
 
